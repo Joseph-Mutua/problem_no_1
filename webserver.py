1d2fe7a4b8e4458fdea5c276333288a8c1637fa4
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-06 23:35:23 +0300

    Finish up basic server

--- webserver.py
+++ webserver.py
@@ -2,4 +2,24 @@
 from http.server import *
 
 # create a class for handling GET and POST requests
- 
+class echoHandler(BaseHTTPRequestHandler):
+     
+    #  Create a function for the GET request
+    def do_GET(self):
+        
+        # Respond with a success status code of 200
+        self.send_response(200)
+        
+        # specify the file type to be send
+        self.send_header("content-type", "text/html")
+        self.end_headers()
+        
+        # The content to be displayed on the web-server
+        self.wfile.write("<h1> My simple Python Webserver</h1>".encode())
+        
+        
+# create an object to take the port number and server-name
+port = HTTPServer(('', 8080), echoHandler)
+
+# Run the server for as long as you like
+port.serve_forever()
